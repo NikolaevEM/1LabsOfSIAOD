70a8fea1cfe0a8521eb825524389810bb440fc86
NikolaevEM <43157756+NikolaevEM@users.noreply.github.com>
2019-12-05 11:24:01 +0900

    Add files via upload

--- Lab4/B.cpp
+++ Lab4/B.cpp
@@ -8,6 +8,13 @@
 #include <fstream>
 using namespace std;
 
+int fact(int n) {
+	int s=1;
+	for (int i=1;i<n+1;i++)
+		s=s*i;
+	return s;
+}
+
 void swaper(char *a, int i1, int i2) {
 	char temp=a[i1];
 	a[i1]=a[i2];
@@ -15,12 +22,25 @@ void swaper(char *a, int i1, int i2) {
 }
 
 void pereborBezA(char *a, int len) {
-	for (int
+	if (len<=2) {  //Если функции дано перебрать 2 элемента, то мы перебираем и печатаем полученные массивы
+		printf("%s",a);
+		cout << endl;
+		swaper(a,0,1);  //Перестановка
+		printf("%s",a);
+		cout << endl;
+		swaper(a,0,1);  //Обратно получаем изначальный массив
+		return;
+	}
+	for (int i=0;i<len;i++) { //На последнее место перебираем все элементы
+		swaper(a,i,len-1); //i-тый элемент меняется с последним элементов
+		pereborBezA(a,len-1);
+		swaper(a,i,len-1);  //Обратно получаем изначальный массив
+	}
 }
 
-int main() { 
+int main() { //frontend
 	char *a;
-	a = new char[8];
+	a = new char[9];
 	int sum;
 	char temp;
 	freopen("input.txt", "r", stdin);  //Чтение файла.
@@ -30,10 +50,13 @@ int main() {
 		len++;
 	}
 	fclose(stdin);
+
+	a[len] = 0;  //Ограничиваем длину массива до len, чтобы при printf не выводился мусор
+
 	//freopen("CON", "r", stdin);  //Перенаправить чтение обратно в консоль.
 	freopen("output.txt", "w", stdout);
-	for (int i=0;i<len;i++)
-		cout << a[i];
+	cout << fact(len) << endl;
+	pereborBezA(a,len);
 	fclose(stdout);
 
 	//freopen("CON", "w", stdout);  //Перенаправить запись обратно в консоль.
