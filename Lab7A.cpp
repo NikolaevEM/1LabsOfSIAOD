0e0bf8893c3008e08a26c0e02979c4facfeab4cc
NikolaevEM <43157756+NikolaevEM@users.noreply.github.com>
2020-02-14 13:20:01 +0900

    Add files via upload

--- Lab7A.cpp
+++ Lab7A.cpp
@@ -4,31 +4,59 @@
 
 using namespace std;
 
-double lenVector(double x, double y) {
-	return sqrt(x*x-y*y);
+int lenVector(int x, int y) {
+	return sqrt(x * x - y * y);
 }
 
-double lenPoints(double x1, double y1, double x2, double y2) {
-	return sqrt(pow((x2-x1),2)+pow((y2-y1),2));
+int lenPoints(int x1, int y1, int x2, int y2) {
+	return sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
 }
 
+bool checker(int xc, int yc, int r, int x, int y) {
+	if (((x - xc) * (x - xc) + (y - yc) * (y - yc)) < r * r) {
+		//Точка принадлежит окружности
+		cout << 0 << endl;
+		return false;
+	}
+	else if (((x - yc) * (x - yc) + (y - yc) * (y - yc)) == r * r) {
+		//Точка лежит на окружности
+		cout << 1 << endl;
+		cout << x << ' ' << y;
+		return false;
+	}
+	else {
+		return true;
+	}
+}
 
 int main() {
-	double xc,yc,r,x,y;
-	double a,h1,h2,b,xb,yb,d,ex,ey,bx,by,h2x,h2y,h3x,h3y;
+	int xc, yc, r, x, y;
+	int a, h1, h2, b, xb, yb, d, ex, ey, bx, by, h2x, h2y, h3x, h3y;
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
 	cin >> xc >> yc >> r >> x >> y;
-	a=lenPoints(x,y,xc,yc);
-	d=sqrt(a*a-r*r);
-	h2=r*d/a;
-	b=r*r/a;
-	ex=(x-xc)/a;
-	ey=(y-yc)/a;
-	bx=ex*b+xc;
-	by=ey*b+yc;
 
-	h2x=-h2*ex;
-	h2y=h2*ey;
-	h3x=h2*ex;
-	h3y=-h2*ey;
-	 //нужно указывать, если вне, на, за
+	if (checker(xc, yc, r, x, y)) {
+		a = lenPoints(x, y, xc, yc);
+		d = sqrt(a * a - r * r);
+		h2 = r * d / a;
+		b = r * r / a;
+		ex = (x - xc) / a;
+		ey = (y - yc) / a;
+		bx = ex * b + xc;
+		by = ey * b + yc;
+
+		h2x = -h2 * ex;
+		h2y = h2 * ey;
+		h3x = h2 * ex;
+		h3y = -h2 * ey;
+
+		cout << 2 << endl;
+		cout << h2x << ' ' << h2y << ' ';
+		cout << h3x << ' ' << h3y << endl;
+	}
+
+	//нужно указывать, если вне, на, за
+	fclose(stdout);
+	return 1;
 }
